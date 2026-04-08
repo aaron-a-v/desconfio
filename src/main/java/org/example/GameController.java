2a220c52497f59913ccbe1356d903d45242fba7a
Aarón <108418490+aaron-a-v@users.noreply.github.com>
2026-04-08 15:51:00 +0200

    Comentarios explicativos del código

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -12,24 +12,24 @@ public class GameController {
 
     public void play(){
         view.showWelcome();
-        String name1 = view.askPlayerName1();
-        String name2 = view.askPlayerName2();
+        String name1 = view.askPlayerName1(); // Guardamos el nombre del jugador 1
+        String name2 = view.askPlayerName2(); // Guardamos el nombre del jugador 2
 
-        p1 = new Player(name1, false);
-        p2 = new Player(name2, false);
-        p3 = new Player("Machine", true);
+        p1 = new Player(name1, false); // Le decimos que el p1 es humano
+        p2 = new Player(name2, false); // Le decimos que el p2 es humano
+        p3 = new Player("Machine", true); // Le decimos que el p3 es la máquina
 
-        this.table = new Table();
-        view.showGreeting(name1);
+        this.table = new Table(); 
+        view.showGreeting(name1); // Le damos la bienvenida a los jugadores
         view.showGreeting(name2);
 
-        table.shuffleDeck();
-        table.distribute(p1, p2, p3);
+        table.shuffleDeck(); // Barajamos la baraja
+        table.distribute(p1, p2, p3); // Repartimos las cartas a cada jugador
 
         // LÓGICA DE JUEGO
         Player[] players = {p1, p2, p3}; // Guardamos los jugadores en un Array para rotarlos
         int turn = 0;
-        boolean gameOver = false;
+        boolean gameOver = false; // Establecemos una variable que nos indicará cuando acaba el bucle
         int currentCardNumber = 1; // El número que se debe jugar
         while (!gameOver) {
 
