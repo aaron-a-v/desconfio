1cf9145f1aa8bc16efcb1a410d7538975a1014e8
Aarón <108418490+aaron-a-v@users.noreply.github.com>
2026-05-12 18:38:12 +0200

    Añadida clase ComputerPlayer para añadir herencia

---
+++ src/main/java/org/example/ComputerPlayer.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.Random;
+
+public class ComputerPlayer extends Player {
+    private Random random = new Random();
+
+    public ComputerPlayer(String name){ super(name);}
+
+
+    @Override
+    public int decidePlay(int numplay) { // La máquina selecciona su jugada
+        // La máquina comprueba si tiene cartas del número que se está jugando
+        int find = -1;
+        for (int i = 0; i < hand.size(); i++) { // La máquina recorre su mano
+            if (hand.get(i).getNum() == numplay) { // Si obtiene de su mano el número que se está jugando piensa
+                find = i;
+                break;
+            }
+        }
+        if (find!=-1) {
+            if (random.nextInt(10) < 8) { // Hay un 80% de probabilidad de que juegue diciendo la verdad
+                return new int[]{find}; // Juega la carta real
+            }
+        }
+        // Si no la tiene o decide mentir por el 20%, juega una carta aleatoria de su mano
+        int randomIndex = random.nextInt(hand.size());
+        return new int[]{randomIndex};
+    }
+
+
+    @Override
+    public boolean decideDistrust(int totalWell){ // La máquina decide si desconfiar
+        // Si el pozo es de 5 cartas o más, la máquina tiene un 75% de probabilidades de desconfiar
+        if (totalWell > 5) {
+            return random.nextInt(100) < 75;
+        } 
+        // Si el pozo es de 5 o menos, mantiene una probabilidad del 50%
+        else {
+            return random.nextBoolean(); // nextBoolean() es equivalente al 50%
+        }
+    }
+
+}
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -5,63 +5,19 @@ import java.util.Random;
 public class Player {
     private String name;
     private ArrayList<Card> hand;
-    private Random random = new Random();
-    private boolean isComputer;
 
-    public Player(String name, boolean b){
+    public Player(String name){
         this.name = name;
-        this.isComputer = b; // Guardamos si es máquina o no
         this.hand = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
-    }
-
+    public String getName() { return name; }
     public ArrayList<Card> getHand(){ return hand;}
+    public void receiveCard(Card cardr){ hand.add(cardr); } // Añadimos cartas a nuestra mano
 
-    public void receiveCard(Card cardr){
-        hand.add(cardr);
-    } // Añadimos cartas a nuestra mano
-
-    public Card extractCard(int index){ // Tiramos una carta
-        return hand.remove(index);
-    }
-
-
-    public int selectCardComputer(int numplay) { // La máquina selecciona su jugada
-        // La máquina comprueba si tiene cartas del número que se está jugando
-        int find = -1;
-        for (int i = 0; i < hand.size(); i++) { // La máquina recorre su mano
-            if (hand.get(i).getNum() == numplay) { // Si obtiene de su mano el número que se está jugando piensa
-                find = i;
-                break;
-            }
-        }
-        if (find!=-1) {
-            if (random.nextInt(10) < 8) { // Hay un 80% de probabilidad de que juegue diciendo la verdad
-                return find; // Juega la carta real
-            }
-        }
-        // Si no la tiene o decide mentir por el 20%, juega una carta aleatoria de su mano
-        return random.nextInt(hand.size());
-    }
-
-
-    public boolean decideDistrust(int totalWell){ // La máquina decide si desconfiar
-        // Si el pozo es de 5 cartas o más, la máquina tiene un 75% de probabilidades de desconfiar
-        if (totalWell > 5) {
-            return random.nextInt(100) < 75;
-        } 
-        // Si el pozo es de 5 o menos, mantiene una probabilidad del 50%
-        else {
-            return random.nextBoolean(); // nextBoolean() es equivalente al 50%
-        }
-    }
-
-    public boolean isComputer() {
-        return isComputer;
-    }
+    // Métodos abstractos que cada tipo de jugador implementará a su manera
+    public abstract int[] decidePlay(int currentRank);
+    public abstract boolean decideDistrust(int totalWell){}
 
     public ArrayList<Card> extractMultipleCards(int[] indices) { // Método que acepte el array de índices
         ArrayList<Card> chosenCards = new ArrayList<>();
